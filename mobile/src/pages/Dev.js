26c75dd289412ea75272857b80a55ee29a567deb
Jean <jeansfloresof@gmail.com>
2019-08-12 00:47:07 -0300

    Configura socket.io client para a plataforma mobile

--- mobile/src/pages/Dev.js
+++ mobile/src/pages/Dev.js
@@ -1,7 +1,10 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import React, { useEffect, useState } from 'react';
 import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import io from 'socket.io-client';
+
 import dislike from '../assets/dislike.png';
+import itsamatch from '../assets/itsamatch.png';
 import like from '../assets/like.png';
 import logo from '../assets/logo.png';
 import api from '../services/api';
@@ -9,6 +12,7 @@ import api from '../services/api';
 const Dev = ({ navigation }) => {
   const id = navigation.getParam('user');
   const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
   useEffect(() => {
     const loadUsers = async () => {
@@ -22,6 +26,16 @@ const Dev = ({ navigation }) => {
     }
 
     loadUsers();
+  }, [id]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333', {
+      query: { user: id }
+    });
+
+    socket.on('match', dev => {
+      setMatchDev(dev)
+    })
   }, [id]);
 
   const handleDislike = async () => {
@@ -79,6 +93,18 @@ const Dev = ({ navigation }) => {
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={handleLike}>
             <Image source={like} />
+          </TouchableOpacity>
+        </View>
+      )}
+
+      { matchDev && (
+        <View style={styles.matchContainer}>
+          <Image style={styles.matchImage} source={itsamatch} />
+          <Image style={styles.matchAvatar} source={{ uri: matchDev.avatar }} />
+          <Text style={styles.matchName}>{matchDev.name}</Text>
+          <Text style={styles.matchBio}>{matchDev.bio}</Text>
+          <TouchableOpacity onPress={() => setMatchDev(null)}>
+            <Text style={styles.closeMatch}>FECHAR</Text>
           </TouchableOpacity>
         </View>
       )}
@@ -162,6 +188,44 @@ const styles = StyleSheet.create({
       width: 0
     }
   },
+  matchContainer: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  matchImage: {
+    height: 60,
+    resizeMode: 'contain'
+  },
+  matchAvatar: {
+    width: 160,
+    height: 160,
+    borderRadius: 80,
+    borderWidth: 5,
+    borderColor: '#FFF',
+    marginVertical: 30
+  },
+  matchName: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#FFF'
+  },
+  matchBio: {
+    marginTop: 10,
+    fontSize: 16,
+    color: 'rgba(255, 255, 255, 0.8)',
+    lineHeight: 24,
+    textAlign: 'center',
+    paddingHorizontal: 30
+  },
+  closeMatch: {
+    fontSize: 16,
+    color: 'rgba(255, 255, 255, 0.8)',
+    textAlign: 'center',
+    marginTop: 30,
+    fontWeight: 'bold'
+  }
 })
 
 export default Dev;
